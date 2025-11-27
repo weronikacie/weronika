27a5c352fa78df84111d8e326ee8ab7c69410925
weronikacie <weronika.cieslak06@gmail.com>
2025-11-27 19:58:24 +0100

    zadanie.py

--- zadanie.py
+++ zadanie.py
@@ -4,5 +4,4 @@ def print_names(name1, name2, name3, name4, name5):
     print(name3)
     print(name4)
     print(name5)
-# Przykład użycia:
 print_names("Julka", "Ola", "Kornelia", "Kuba", "Marek")
