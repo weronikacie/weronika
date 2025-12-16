13f46542b711c245be4c0c518563af21e6b19c03
Weronika <weronika.cieslak06@gmail.com>
2025-12-16 15:35:11 +0100

    Zadania oop 1-3

---
+++ oop/zad_1.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Student:
+    def __init__(self, name: str, marks: List[int]):
+        self.name = name
+        self.marks = marks
+
+    def is_passed(self) -> bool:
+        if not self.marks:
+            return False
+
+        average = sum(self.marks) / len(self.marks)
+        return average > 50
+
+
+student1 = Student("Weronika", [60, 50, 70])
+student2 = Student("Ola", [40, 70, 30])
+
+print(student1.name, student1.is_passed())
+print(student2.name, student2.is_passed())
+
+
+
