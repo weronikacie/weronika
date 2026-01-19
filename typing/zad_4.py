19a04b2c2bb0b164d923423c164f5ed7c488188f
weronikacie <weronika.cieslak06@gmail.com>
2026-01-19 11:51:28 +0100

    usunięcie plików

--- typing/zad_4.py
+++
@@ -1,9 +0,0 @@
-def check_sum(a: int, b: int, c: int) -> bool:
-    return a + b == c
-
-
-result = check_sum(5, 7, 10)
-if result:
-    print("Suma dwóch pierwszych liczb jest większa lub równa trzeciej")
-else:
-    print("Suma dwóch pierwszych liczb jest mniejsza od trzeciej")
